950a5eadcbd97fd13e8d52e8d80881f5a0951731
GOPIM06 <gopimurugan552@gmail.com>
2025-09-09 12:44:20 +0530

    Delete spring-boot-demo directory

--- spring-boot-demo/src/main/java/com/op/spring_boot_demo/model/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.op.spring_boot_demo.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<user_entity, Long> { }
